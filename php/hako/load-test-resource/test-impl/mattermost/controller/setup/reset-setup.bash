8eaedb87753dcb980551eb11a5c7f9e65aaf6bdd
Takashi Mori <kanetugu2015@gmail.com>
2022-10-05 09:04:54 +0900

    add user test

---
+++ php/hako/load-test-resource/test-impl/mattermost/controller/setup/reset-setup.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ -z ${TEST_TARGET} ]
+then
+    source env/env.bash
+fi
+
+source ${TEST_LOGGER}
+
+tlog "SETUP TEST..."
+bash test-utils/remote_script.bash ${MATTERMOST_TOOL_DIR}/bin/mm-stop.bash ${MATTERMOST_TOOL_DIR}
+bash test-utils/remote_script.bash ${MATTERMOST_TOOL_DIR}/bin/mm-reset.bash ${MATTERMOST_TOOL_DIR}
+
